b89b44278979616a38431dd9465dbd3f4d235b74
financialfly <1012593988@qq.com>
2020-01-16 17:30:09 +0800

    Update decision-tree.py

--- tl-code/decision-tree.py
+++ tl-code/decision-tree.py
@@ -390,14 +390,13 @@ if __name__ == '__main__':
     from sklearn.datasets import load_iris
     from sklearn.model_selection import train_test_split
 
-    if __name__ == "__main__":
-        # 分类树
-        X, y = load_iris(return_X_y=True)
+    # 分类树
+    X, y = load_iris(return_X_y=True)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier()
 
-        clf.fit(X_train, y_train)
+    clf.fit(X_train, y_train)
 
-        print("Classifier Score:", clf.score(X_test, y_test))
+    print("Classifier Score:", clf.score(X_test, y_test))
